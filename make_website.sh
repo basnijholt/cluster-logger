317669497dd69fc732873bcfda73df3871243cb9
Bas Nijholt <basnijholt@gmail.com>
2017-08-04 11:51:21 +0200

    Use bas@hpc05 key to upload hpc05.quantumtinkerer.tudelft.nl

--- make_website.sh
+++ make_website.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 jupyter nbconvert --to html --execute --ExecutePreprocessor.timeout=600 --log-level WARN index.ipynb >> error.log 2>&1
-rsync -ravz index.html hpc05@tnw-tn1.tudelft.net:
-rsync -ravz database.p hpc05@tnw-tn1.tudelft.net:
+rsync -e "ssh -i $HOME/.ssh/id_rsa_hpc05.pub" -ravz index.html hpc05@tnw-tn1.tudelft.net:
+rsync -e "ssh -i $HOME/.ssh/id_rsa_hpc05.pub" -ravz database.p hpc05@tnw-tn1.tudelft.net:
 rm -f index.html
