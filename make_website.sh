2d9b626af48efc582f068367c8e564f02fbb412b
Bas Nijholt <basnijholt@gmail.com>
2018-10-29 12:53:22 +0100

    increase timeout

--- make_website.sh
+++ make_website.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-jupyter nbconvert --to html --execute --ExecutePreprocessor.timeout=600 --log-level WARN index.ipynb >> error.log 2>&1
+jupyter nbconvert --to html --execute --ExecutePreprocessor.timeout=3000 --log-level WARN index.ipynb >> error.log 2>&1
 rsync -ravz index.html hpc05@tnw-tn1.tudelft.net:
 rsync -ravz database.p hpc05@tnw-tn1.tudelft.net:
 rm -f index.html
